43a62232825cde77f463a7a8195cd5e874a4d843
riddhi <riddhitaharabadkar@gmail.com>
2025-05-31 11:37:26 +0530

    port binding

--- main.py
+++ main.py
@@ -103,4 +103,6 @@ def matcher():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
